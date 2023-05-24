bfde7e2d8dd25b706f83377a6bfd451077cb32e8
Martynas <martynas@sdhouse.lt>
2023-05-24 19:06:29 +0300

    Another baser setup

---
+++ config.js
@@ -0,0 +1,7 @@
+require("dotenv").config();
+
+module.exports = {
+  token: process.env.DISCORD_BOT_TOKEN_SECRET,
+  clientId: process.env.DISCORD_BOT_APPLICATION_ID,
+  guildId: process.env.SPAMY_BOY_SERVER_ID,
+};
--- main.js
+++ main.js
@@ -1,12 +1,47 @@
-require("dotenv").config();
+const { Client, Events, GatewayIntentBits } = require("discord.js");
+const { initiateCommands } = require("./initiate_commands");
+const { token } = require("./config");
 
-const { Client, GatewayIntentBits } = require("discord.js");
 const client = new Client({
-  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.DirectMessages,
+    GatewayIntentBits.GuildMessages,
+  ],
+  partials: ["MESSAGE", "CHANNEL"],
 });
 
-client.on("ready", () => {
+client.login(token);
+
+client.on(Events.ClientReady, () => {
   console.log("The AI bot is online"); //message when bot is online
 });
 
-client.login(process.env.DISCORD_BOT_TOKEN_SECRET);
+initiateCommands(client);
+
+client.on(Events.InteractionCreate, async (interaction) => {
+  if (interaction.isChatInputCommand()) {
+    console.log("interaction", interaction);
+
+    const command = interaction.client.commands.get(interaction.commandName);
+
+    if (command) {
+      try {
+        await command.execute(interaction);
+      } catch (error) {
+        console.error("error", error);
+
+        const errorResponse = {
+          content: "There was an error while executing this command!",
+          ephemeral: true,
+        };
+
+        if (interaction.replied || interaction.deferred) {
+          await interaction.followUp(errorResponse);
+        } else {
+          await interaction.reply(errorResponse);
+        }
+      }
+    }
+  }
+});
